8979c9fde1e6986e0c5ae78757794c8fbe9d8f61
Stas Lobanov <stas.lbv@gmail.com>
2017-02-06 11:28:14 +0200

    Update code

--- playground/dblite.js
+++ playground/dblite.js
@@ -10,7 +10,7 @@ var env       = (process.env.NODE_ENV || 'development');
 var sequelize;
 
 if (env == 'production'){ // only on heroku
-    sequelize = new Sequelize(process.env.DATABASE_URL,{
+    sequelize = new Sequelize(undefined,undefined,process.env.DATABASE_URL,{
     	dialect: 'postgres'
     });
 }else{
